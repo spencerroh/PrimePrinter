c7ce368b0e63cb1db9c04ede42f5e103ef8695cf
Spencer Roh <yt1219.roh@samsungmedison.com>
2021-06-11 16:44:44 +0900

    실습 예제

---
+++ PrimePrinterTest/PrimePrinterMatchTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PrimePrinterTest
+{
+    [TestClass]
+    public class PrimePrinterMatchTest
+    {
+        [TestMethod]
+        public void MakeSureOutputMatchesGold()
+        {
+            TextWriter ConsoleOutStream = Console.Out;
+            Console.SetOut(new StreamWriter(File.Open("lead.txt", FileMode.Create)));
+
+            PrimePrinter.Program.Main(null);
+
+            Console.Out.Close();
+            Console.SetOut(ConsoleOutStream);
+            
+
+            StreamReader lead = null;
+            StreamReader gold = null;
+
+            try
+            {
+                lead = new StreamReader(File.Open("lead.txt", FileMode.Open));
+                gold = new StreamReader(File.Open("..\\..\\gold.txt", FileMode.Open));
+
+                while (!lead.EndOfStream && !gold.EndOfStream)
+                {
+                    Assert.AreEqual(
+                        lead.ReadLine(),
+                        gold.ReadLine());
+                }
+            }
+            finally
+            {
+                lead.Close();
+                gold.Close();
+            }
+        }
+    }
+}
